2cdd66b157d41061fdb449d2f791bbad37d51248
Mostak Khan <Mostak1993@gmail.com>
2025-07-02 17:13:27 +0100

    style changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
 import Heading from '@/components/Heading'
 import { shoppath } from '@/paths'
 
 const Home = () => {
+
   return (
     <div className="bg-gradient-to-r from-[#d19e74] to-[#50341d] h-screen">
-      <div className="grid grid-cols-2 items-center justify-center h-full">
-        <div  className='flex flex-col gap-3 items-center justify-center'>
+      <div className="flex flex-col  gap-3 md:grid md:grid-cols-2 items-center justify-around h-full">
+        <div className="flex flex-col items-center justify-center gap-[20px]">
           <Image
-            src={"/next.svg"}
+            src={'/next.svg'}
             width={256}
             height={256}
             alt="Ecommerce Logo"
-            className='object-cover'
+            className="object-cover"
           />
           <Heading title="Commerce" description="Welcome to my Shop" />
         </div>
